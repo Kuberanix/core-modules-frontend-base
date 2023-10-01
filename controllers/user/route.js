f980914505f7ed210dd52bbc150271a647c2b33e
Rakesh Dhariwal <rakeshdhariwal61@gmail.com>
2023-10-01 10:57:21 +0530

    clean/code clean

--- controllers/user/route.js
+++
@@ -1,8 +0,0 @@
-const users = [
-  { name: 'ABc', age: 17 },
-  { name: 'DEF', age: 19 },
-];
-function GET(req, res) {
-  return res.status(200).json(users);
-}
-export { GET as GETUSERS };
--- routes/users/routes.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import { GETUSERS } from '#controllers/user/route.js';
-const router = express.Router();
-router.get('/', GETUSERS);
-export default router;
